112f3cfe05d47fb07bccbe5d42bd8d7046557bcf
dalkiran1907 <dlkrnsule1907@gmail.com>
2023-03-25 15:57:38 +0300

    menu

---
+++ src/components/text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const text = () => {
+  return (
+    <div className='text' >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, neque et ullam distinctio nulla incidunt praesentium atque ipsam similique eaque numquam doloribus? Itaque quis doloribus natus quidem. Excepturi consectetur pariatur consequuntur nostrum deserunt, vel nobis rerum possimus error praesentium! Ratione modi, eos ea dolorum beatae delectus. Vel sit modi fugit in, ipsum dolore ipsa architecto tempora natus, optio repudiandae aspernatur doloribus itaque voluptatem harum reprehenderit quod ducimus? Libero, fuga nostrum tempora quibusdam amet recusandae est facilis cumque repudiandae, ipsam vitae doloribus natus optio beatae maxime ea ipsum aliquam dolorum deserunt fugiat ut maiores officiis. Blanditiis porro officiis suscipit culpa quia?</p>
+    </div>
+  )
+}
+
+export default text
